cb3c22a49706da1296d659d677ee341c7074abc9
michael-ola <michael303.mi@gmail.com>
2025-01-31 14:43:58 +0100

    first commit

---
+++ induction_parameter.m
@@ -0,0 +1,26 @@
+%%Simulation of Induction motor Parameters
+
+r_s=79.13;
+r_r=40.5;
+
+
+%Inductance
+
+Lls=4.928-4.625;
+Lms=4.625;
+Llr=Lls;
+Lm=Lms;
+
+
+%Moment of Inertia
+J=0.0038*1.5;
+% Load Torque
+%Tl=0;
+%Viscous Contant
+Bm=0.00071*2;
+
+%Supply Voltage
+Vm=310;
+
+omega=0;
+p=4;
